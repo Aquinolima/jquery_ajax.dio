40be3ba306b2edbc9a1737d977c4d703e14962a5
Thiago Aquino <aquinolima.thiago@gmail.com>
2022-06-24 00:36:16 -0300

    Validação Cep por Ajax com Jquery - FINISH

--- js/main.js
+++ js/main.js
@@ -1,19 +1,28 @@
+function delay(n){
+    return new Promise(function(resolve){
+        setTimeout(resolve,n*1000);
+    });
+}
 
-function consultaCep(){
+async function consultaCep(){
+    
+    $("#loader").show();                
     var cep = document.getElementById("cep").value;
     var url = "https://viacep.com.br/ws/" + cep + "/json/";
     console.log(url);    
+    await delay(1);
     $.ajax({
         url: url,
         type: "GET",
         success: function(response){
-           // console.log(response);
+            // console.log(response);
             $("#num_cep").html(response.cep);
             $("#logradouro").html(response.logradouro);
             $("#uf").html(response.uf);
             $("#localidade").html(response.localidade);
             $("#bairro").html(response.bairro);
-            $("#resultado").show();                
+            $("#loader").hide();                
+            $("#resultado").show();
         }
     })
 }
